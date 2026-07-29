7792b2f6164cde378db162d8988439d7790e709e
Sourabh0777 <sourabh.verma9770@gmail.com>
2026-07-29 12:33:48 +0000

    gin

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+
+	"learning-go-shop/internal/config"
+	"learning-go-shop/internal/database"
+	"learning-go-shop/internal/logger"
+)
 
 func main() {
 	fmt.Println("this is print")
+	log := logger.New()
+	cfg, err := config.Load()
+	if err != nil {
+		log.Fatal().Err(err).Msg("Failed to load config")
+	}
+	db, err := database.New(&cfg.Database)
+	if err != nil {
+		log.Fatal().Err(err).Msg("failed to connect to database")
+	}
+	mainDB, err := db.DB()
+	if err != nil {
+		log.Fatal().Err(err).Msg("Failed to get database connection")
+	}
+	defer mainDB.Close()
+	gin.SetMode(cfg.Server.GinMode)
+	log.Info().Msg("starting server")
 }
