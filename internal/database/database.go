d370996567b9dbc872ff5aa474689e6e8332d3a1
Sourabh0777 <sourabh.verma9770@gmail.com>
2026-07-29 03:44:01 +0000

    add PostgreSQL driver and GORM integration for database connection

--- internal/database/database.go
+++ internal/database/database.go
@@ -1,1 +1,29 @@
 package database
+
+import (
+	"fmt"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+
+	"learning-go-shop/internal/config"
+)
+
+func New(cfg *config.DatabaseConfig) (*gorm.DB, error) {
+	dsn := fmt.Sprintf(
+		"host=%s user=%s password=%s dbname=%s port=%s sslmode=%s",
+		cfg.Host,
+		cfg.User,
+		cfg.Password,
+		cfg.Port,
+		cfg.SSLMode,
+	)
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{
+		Logger: logger.Default.LogMode(logger.Info),
+	})
+	if err != nil {
+		return nil, fmt.Errorf("failed to connect to database: %w", err)
+	}
+	return db, nil
+}
