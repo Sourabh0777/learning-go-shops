30541edc65a11409b12428a158bea0bb588ddea6
Sourabh0777 <sourabh.verma9770@gmail.com>
2026-07-31 06:56:41 +0000

    implement server structure and router initialization

--- internal/server/server.go
+++ internal/server/server.go
@@ -1,1 +1,29 @@
 package server
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/rs/zerolog"
+	"gorm.io/gorm"
+
+	"learning-go-shop/internal/config"
+)
+
+type Server struct {
+	config *config.Config
+	db     *gorm.DB
+	logger zerolog.Logger
+}
+
+func New(cfg *config.Config, db *gorm.DB, logger zerolog.Logger) *Server {
+	return &Server{
+		config: cfg,
+		db:     db,
+		logger: logger,
+	}
+}
+
+func (s *Server) Router() *gin.Engine {
+	router := gin.New()
+
+	return router
+}
