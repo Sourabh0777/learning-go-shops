9966e7c559d7a7d30fcdfe127763a7e8e91c2764
Sourabh0777 <sourabh.verma9770@gmail.com>
2026-08-22 09:02:23 +0000

    Add user profile routes with authentication middleware

--- internal/server/server.go
+++ internal/server/server.go
@@ -39,6 +39,21 @@ func (s *Server) SetupRoutes() *gin.Engine {
 			auth.POST("/login", s.login)
 			auth.POST("/refresh", s.refreshToken)
 			// auth.POST("/logout", s.register)1
+
+			{
+				protected := auth.Group("/")
+				protected.Use(s.authMiddleware())
+				{
+					users := protected.Group("users")
+					{
+						userRoutes := users
+						userRoutes.GET("/profile", s.getProfile)
+						userRoutes.POST("/profile", s.updateProfile)
+					}
+
+				}
+			}
+
 		}
 	}
 	// asdsdasd
